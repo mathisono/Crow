15940941a3e6d1d8a15df294f9d4703e1cbf2666
Builder Bob <mathison@aredn-rfeye.local>
2026-06-20 15:17:55 -0700

    fix: use env command for UCODE_REQUIRE_PATH in wrapper
    Replace export/exec approach with 'env' command which more reliably passes
    environment variables to child process under procd management.

--- platforms/aredn/crow-runner.sh
+++ platforms/aredn/crow-runner.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 # Crow runner wrapper - sets UCODE_REQUIRE_PATH and executes router.uc
 # This is necessary for AREDN ucode module resolution
-
-export UCODE_REQUIRE_PATH="/usr/share/ucode/?.uc:/usr/local/crow/?.uc"
-exec /usr/bin/ucode /usr/local/crow/router.uc "$@"
+exec env UCODE_REQUIRE_PATH="/usr/share/ucode/?.uc:/usr/local/crow/?.uc" /usr/bin/ucode /usr/local/crow/router.uc "$@"
